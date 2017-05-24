2e6fcb771c1476a8f8149bf2641f57dacd50f92f
Lucie <lucie.perrotta@epfl.ch>
2017-05-24 21:50:06 +0200

    queries 3 almost done

--- queries/querries milestone 3.sql
+++ queries/querries milestone 3.sql
@@ -1,24 +1,29 @@
--- a) changer un peu
-SELECT 	name, 
-		nb
-FROM 	series INNER JOIN
-   		(SELECT series_id, 
-   				COUNT(*) AS nb
-    	FROM 	issue INNER JOIN 
-        		(SELECT distinct issue_id as iid
-        		FROM 	story
-        		WHERE 	type_id NOT IN
-	            		(SELECT type_id
-	            		FROM 	story
-	           			GROUP BY type_id
-	            		ORDER BY count(*) DESC LIMIT 1
+-- a) ok
+SELECT 	S.name, 
+		T2.nbi
+FROM 	(
+   		SELECT 	I.series_id, 
+   				COUNT(*) AS nbi
+    	FROM 	issue I,
+        		(
+        		SELECT distinct S.issue_id
+        		FROM 	story S
+        		WHERE 	S.type_id <>
+	            		(
+	            		SELECT 	S.type_id
+	            		FROM 	story S
+	           			GROUP BY S.type_id
+	            		ORDER BY COUNT(*) DESC LIMIT 1
 	            		)
-            	) st ON issue.id=st.iid
-    			GROUP BY series_id
-    	) ON ID=series_id
-ORDER BY db DESC
+            	) as T 
+        WHERE	I.id = T.issue_id
+    	GROUP BY I.series_id
+    	) as T2,
+    	series S
+WHERE 	S.id = T2.series_id
+ORDER BY T2.nbi DESC
 
--- b)
+-- b) ok (improved = hardcode 3)
 SELECT 	P.name
 FROM 	publisher P
 WHERE 	(SELECT COUNT(distinct SP.name)
@@ -28,47 +33,26 @@ WHERE 	(SELECT COUNT(distinct SP.name)
 				S.publisher_id = P.id
 	 	) = 3
 
--- c)
+-- c) improved (no story access)
 SELECT C.name
 FROM	(
 		SELECT	HC.character_id,
 				COUNT(*) as nch
 		FROM 	story_reprint SR,
-				story S,
 				artist A, 
 				has_script HS, 
 				has_characters HC
-		WHERE 	SR.origin_id = S.id AND
-				HS.story_id = S.id AND
+		WHERE 	HC.story_id = SR.origin_id AND
+				HC.story_id = HS.story_id AND
 				HS.artist_id = A.id AND
-				A.name LIKE '%Alan_Moore%' AND
-				HC.story_id = S.id
+				A.name LIKE '%Alan_Moore%'	
 		GROUP BY HC.character_id
 		) as T,
 		characters C
 WHERE	C.id = T.character_id
 ORDER BY T.nch DESC LIMIT 10
 
--- c) improved (no story)
-SELECT C.name
-FROM	(
-		SELECT	HC.character_id,
-				COUNT(*) as nch
-		FROM 	story_reprint SR,
-				artist A, 
-				has_script HS, 
-				has_characters HC
-		WHERE 	HS.artist_id = A.id AND
-				A.name LIKE '%Alan_Moore%' AND
-				HC.story_id = SR.origin_id AND
-				HC.story_id = HS.story_id
-		GROUP BY HC.character_id
-		) as T,
-		characters C
-WHERE	C.id = T.character_id
-ORDER BY T.nch DESC LIMIT 10
-
--- d)
+-- d) ok
 SELECT 	distinct A.name
 FROM 	artist A, 
 		has_script HS
@@ -91,40 +75,25 @@ WHERE	HS.artist_id = A.id AND
 				S.synopsis LIKE '%natur%')
 		)
 
--- e) hellp
-SELECT	T.name
-FROM	(
-		SELECT	P.id,
-				COUNT(*) as num
-		FROM	series S,
-				publisher P
-		WHERE	S.publisher_id = P.id
-		GROUP BY P.id
-		) as T
-ORDER BY T.num DESC LIMIT 10
+-- e) ok
+SELECT 	L.name,
+		COUNT(*) as nb 
+FROM 	series SE LEFT JOIN 
+		( 
+		SELECT 	P.id, 
+				COUNT(P.id) 
+		FROM 	series S,
+				publisher P 
+		WHERE 	P.id = S.publisher_id
+		GROUP BY P.id DESC 
+		ORDER BY COUNT(P.id) DESC LIMIT 10
+		) as T ON T.id = SE.publisher_id,
+		language L 
+WHERE	SE.language_id = L.id 
+GROUP BY L.id 
+ORDER BY nb DESC LIMIT 3
 
-so long: 10 publishier name in order by series
-
--- eee 2
-SELECT	T.pid, 
-		T.num, 
-		SS.language_id
-FROM	series SS,
-		SELECT	T.pid, 
-				T.num
-		FROM	(
-				SELECT	P.id,
-						COUNT(*) as num
-				FROM	series S,
-						publisher P
-				WHERE	S.publisher_id = P.id
-				GROUP BY P.id
-				) as T
-		ORDER BY T.num LIMIT 10
-WHERE	T.pid = SS.publisher_id
-
-
--- f)
+-- f) ok
 SELECT	T.name, 
 		T.num
 FROM	(
@@ -146,91 +115,101 @@ FROM	(
 WHERE 	T.num >= 10000
 ORDER BY T.num DESC
 
--- g)
+-- g) ok
 SELECT 	distinct STT.name
-		country C,
-FROM 	language L,
-		series SE,
+FROM	(
+		SELECT	ST.type_id
+		FROM	(
+				SELECT	I.id
+				FROM	series SE,
+						issue I
+				WHERE	SE.id = I.series_id AND
+						SE.country_id <> 51 AND
+						SE.publication_type_id = 2
+				) as T1,
+				story ST
+		WHERE	T1.id = ST.issue_id
+		) as T2,
+		story_type STT
+WHERE	STT.id = T2.type_id
+
+--g) ok (improve = hardcore 51 and 2)
+SELECT 	distinct STT.name
+FROM	series SE,
 		story ST, 	
 		issue I,
 		story_type STT
-WHERE	NOT C.name = 'Italy' AND
-		C.id = SE.country_id AND
-		SE.id = I.series_id AND
+WHERE	STT.id = ST.type_id AND
 		I.id = ST.issue_id AND
-		SE.publication_type_id = 2 AND
-		STT.id = ST.type_id
+		SE.id = I.series_id AND
+		SE.country_id <> 51 AND
+		SE.publication_type_id = 2
+		
+-- g) Dom version
+SELECT distinct STT.name 
+FROM story ST
+LEFT JOIN story_type STT ON STT.id = ST.type_id
+LEFT JOIN (
+		SELECT I.id 
+		FROM issue I 
+		RIGHT JOIN (
+					SELECT SE.id 
+					FROM series SE
+					WHERE SE.language_id != 51 AND 
+					SE.publication_type_id = 2
+					) AS temp ON temp.id = I.series_id
+		) AS iss ON ST.issue_id = iss.id
 
--- h)
-SELECT distinct A.name FROM artist A, has_script HS, Story S, Story_type ST
-WHERE 
-  A.id = HS.artist_id AND
-  HS.story_id = S.id AND
-  S.type_id = ST.id AND
-  ST.name = "cartoon"
-HAVING
-  1 =< COUNT(
+-- h) improvement = hardcode type_id=5
+SELECT 	A.name
+FROM 	artist A, 
+		has_script HS, 
+		story S,
+		issue I,
+		indicia_publisher IP
+WHERE 	A.id = HS.artist_id AND
+  		HS.story_id = S.id AND
+  		S.type_id = 5 AND
+  		S.issue_id = I.id AND
+  		I.indicia_publisher_id = IP.id
+GROUP BY A.name
+HAVING COUNT(*) > 1
 
-SELECT IP.id FROM artist A, has_script HS, story S, issue I, series SE, indicia_publisher IP, publisher P
-WHERE
-  IP.publisher_id = P.id AND
-  SE.publisher_id = P.id AND
-  I.series_id = SE.id AND
-  S.issue_id = I.id AND
-  HS.story_id = S.id AND
-  A.id = HS.artist_id
-GROUP BY IP.id
-
-)
-
--- i) meh
-SELECT	T.name,
+-- i) ok
+SELECT	distinct BG.name,
 		COUNT(*) as ipn
-FROM	(
-		SELECT	distinct BG.name, 
-				IP.id
-		FROM	indicia_publisher IP, 
-				publisher P,
-				brand_group BG
-		WHERE	BG.publisher_id = P.id AND
-				IP.publisher_id = P.id
-		) AS T
-GROUP BY T.name
+FROM	indicia_publisher IP, 
+		brand_group BG
+WHERE	BG.publisher_id = IP.publisher_id
+GROUP BY BG.name
 ORDER BY ipn DESC LIMIT 10
 
--- j)
+-- j) ok
 SELECT 	T.name,
-		AVG(years) AS lgh
+		AVG(years) AS average_years
 FROM	(
 		SELECT	distinct I.name, 
 				(S.year_ended - S.year_began) AS years
 		FROM	series S,
 				indicia_publisher I
-		WHERE	S.publisher_id = I.publisher_id AND
-				S.year_began < S.year_ended AND 
+		WHERE	S.year_began < S.year_ended AND 
 		  		S.year_began > 0 AND 
-		  		S.year_ended > 0
+		  		S.year_ended > 0 AND
+		  		S.publisher_id = I.publisher_id 
 		) AS T
 GROUP BY T.name
-ORDER by lgh DESC
 
--- k) 
-SELECT	I.name,
+-- k) ok
+SELECT	I.name, 
 		COUNT(*) as nb
-FROM	(
-		SELECT	I.id
-		FROM	series S, 
-				publisher P,
-				indicia_publisher I
-		WHERE	S.first_issue_id = S.last_issue_id AND
-				S.publisher_id = P.id AND
-				I.publisher_id = P.id
-		) as T,
+FROM	series S,
 		indicia_publisher I
+WHERE	S.first_issue_id = S.last_issue_id AND
+		S.publisher_id = I.publisher_id
 GROUP BY I.name
 ORDER BY nb DESC LIMIT 10
 
--- l)
+-- l) ok
 SELECT	S.id, 
 		IP.name, 
 		COUNT(*) as nb
@@ -244,24 +223,32 @@ WHERE	HS.story_id = S.id AND
 GROUP BY S.id, IP.name
 ORDER BY nb DESC LIMIT 10
 
--- m)
+-- m) empty result
 SELECT	distinct C.name
 FROM	characters C,
 		has_characters HC,
-		story S,
-		issue I,
-		indicia_publisher IP
-WHERE 	C.id = HC.character_id AND
-		S.id = HC.story_id AND
-		S.issue_id = I.id AND
-		I.indicia_publisher_id = IP.id AND
-		IP.name LIKE '%Marvel%DC%' AND
-		S.features LIKE C.name
-
--- n)
+		has_featured_characters HFC,
+		story S1,
+		story S2,
+		issue I1,
+		issue I2,
+		indicia_publisher IP1,
+		indicia_publisher IP2
+WHERE 	HC.character_id = C.id AND
+		HC.story_id  = S1.id AND
+		HFC.character_id = C.id AND
+		HFC.story_id = S2.id AND
+		S1.issue_id = I1.id AND
+		I1.indicia_publisher_id = IP1.id AND
+		IP1.name LIKE 'Marvel' AND
+		S2.issue_id = I2.id AND
+		I2.indicia_publisher_id = IP2.id AND
+		IP2.name LIKE '%Marvel%DC%'
+		
+-- n) ok
 SELECT	T.name
 FROM	(
-		SELECT	distinct S.name,
+		SELECT	S.name,
 				COUNT(*) as inb
 		FROM	series S,
 				issue I
@@ -270,13 +257,12 @@ FROM	(
 		) AS T
 ORDER BY T.inb DESC LIMIT 5
 
--- o) help, how to group by on 2 levels
-SELECT	I.title,
-		S.title
-FROM 	issue I, 
-		story S, 
+-- o) ok
+SELECT	S.title,
+		COUNT(*) as nb
+FROM 	story S, 
 		story_reprint SR
-WHERE	S.issue_id = I.id AND
+WHERE	S.issue_id = 68 AND
 		S.id = SR.origin_id
-
-all issues with their reprinted stories
+GROUP BY S.title
+ORDER BY nb DESC LIMIT 1
